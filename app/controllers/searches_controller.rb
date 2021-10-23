7267ec911af1932b748b7969b929df41809769ce
lion-oldriver <w879443a72cj2i@gmail.com>
2021-10-23 06:50:53 +0000

    [ORsearch]

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,19 +1,22 @@
 class SearchesController < ApplicationController
   def search
-    @model = params["model"]
-    @content = params["content"]
-    @records = search_for(@model, @content).includes(:shrine_gods, :gods, :shrine_tags, :tags).page(params[:page]).per(10)
-  end
-
-  private
-
-  def search_for(model, content)
+    model = params[:model]
+    contents = params[:content].split(/[[:blank:]]+/).select(&:present?) # 検索ワードを分割する
+    @shrines = Shrine.none # 空のモデルオブジェクトを作成
+    # プルダウンで選択した値で場合分けを行いOR検索を行う
     if model == "shrine"
-      Shrine.where('name LIKE ?', '%' + content + '%') # 神社の名前で部分一致検索
+      contents.each do |content|
+        @shrines = @shrines.or(Shrine.where("name LIKE ?", "%#{content}%").includes(:shrine_gods, :gods, :shrine_tags, :tags))
+      end
     elsif model == "god"
-      Shrine.joins(:gods).where('god_name LIKE ?', '%' + content + '%').distinct # 神様の名前で部分一致検索,重複レコードはまとめる
+      contents.each do |content|
+        @shrines = @shrines.or(Shrine.joins(:gods).where("god_name LIKE ?", "%#{content}%").includes(:shrine_gods, :gods, :shrine_tags, :tags))
+      end
     elsif model == "tag"
-      Shrine.joins(:tags).where('tag_name LIKE ?', '%' + content + '%').distinct # タグで部分一致検索,重複レコードはまとめる
+      contents.each do |content|
+        @shrines = @shrines.or(Shrine.joins(:tags).where("tag_name LIKE ?", "%#{content}%").includes(:shrine_gods, :gods, :shrine_tags, :tags))
+      end
     end
+    @shrines = Kaminari.paginate_array(@shrines).page(params[:page]).per(8) # 配列用のページネーション
   end
 end
